1cab7a60110348071f32b93f6a8eda63b8757f5d
Carrie Kraft <carriekarft@gmail.com>
2022-12-03 05:26:46 -0800

    commit tree 8f35c26d8c5af8b0742d74f6e8b86639aa928e91

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+let tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -12,5 +12,44 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((item) => {
+    let words = item.split(" ")
+    const firstPartOfWords = words.map((words) => words.charAt(0).toUpperCase() + words.slice(1))
+    const tutorialsTitles = firstPartOfWords.join(" ")
+    return tutorialsTitles;
+    
+    
+  })
 }
+titleCased()
+
+/*const titleCased = () => {
+  return tutorials.map((splittingItUp) => splittingItUp.charAt(0).toUpperCase());
+  
+}*/
+// console.log(titleCased())
+
+/*const tryingThinsOut = () => {
+  const newString = tutorials.join(", ");
+  const wereGettingWeird = newString.toString().split(" ")
+  const lastBitOfWord = wereGettingWeird.map((item) => item.slice(1))
+  // console.log(lastBitOfWord)
+  const tutorailFirstLetter = wereGettingWeird.map((titleWord) => titleWord.charAt(0).toUpperCase());
+  tutorials = tutorailFirstLetter + lastBitOfWord
+  console.log(tutorials)
+ 
+  
+}
+titleCased()(/)
+
+/*function splittingUp(array) {
+  const newTutorials =  array.map((title) => title.split(" "))
+  return newTutorials
+}
+splittingUp(tutorials);*/
+
+/*const tryingThisOut = array => {
+  const arr =  array.split(' ');
+  return arr
+}
+console.log(tryingThisOut(tutorials));*/
